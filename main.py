c65136b0f8677d0d7350822c35f5f62effdc7116
Sudhanshu <kesharwani.sudhanshu1990@gmail.com>
2023-06-17 20:25:19 +0530

    upgraded

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+# ________________***PDF Generation using data from CSV***________________
 
 from fpdf import FPDF
 
 pdf = FPDF(orientation='p', unit="mm", format="A4")
 
-pdf.add_page()
-pdf.set_font(family="Arial", style="B", size=12)
-pdf.cell(w=0, h=12, txt="Lanos institute of computer science", border=1, ln=1)
-pdf.cell(w=0, h=12, txt="Learn and liberate your future", border=0, ln=1)
 
-pdf.rect(50,50,100,50, 'D', )
+pdf.set_font(family="Arial", style="B", size=12)
+
+import pandas as pd
+
+df_pdf = pd.read_csv("003 topics.csv", sep=",")
+
+for index, row in df_pdf.iterrows():
+    topic = row["Topic"]
+    pdf.add_page()
+    pdf.cell(w=0, h=12, txt=topic, border=0,ln=1)
+
+pdf.output("output.pdf")
